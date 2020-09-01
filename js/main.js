ba92aa44d7cdba029a7d3617376f664d55510fd9
Takuto Arima <ta910arima@gmail.com>
2020-09-01 10:25:36 +0900

    練習

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+{
+let numbers = [1,2,3];
+let doubled = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   doubledNumbers.push(numbers[i] * 2);
+// }
+
+let doubl = numbers.map(function(number) {
+  return number * 2;
+});
+
+
+console.log (doubl);
+
+
+
+
+
+}
